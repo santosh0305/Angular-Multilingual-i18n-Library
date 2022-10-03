7e97ce94bf16f53a5455c08273654c3dfcaef52d
santosh0305 <32795188+santosh0305@users.noreply.github.com>
2022-10-03 17:19:02 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,17 @@ import { TranslateService } from '@ngx-translate/core';
   selector: 'app-root',
   template: `
     <div>
-      <h2>{{ 'HOME.TITLE' | translate }}</h2>
+      
       <label>
         {{ 'HOME.SELECT' | translate }}
         <select #langSelect (change)="translate.use(langSelect.value)">
           <option *ngFor="let lang of translate.getLangs()" [value]="lang" [selected]="lang === translate.currentLang">{{ lang }}</option>
         </select>
+      </label>
+      <h2>{{ 'HOME.TITLE' | translate }}</h2>
+      <div></div>
+      <label>
+        {{ 'ERROR.INVALIDCART'  | translate }}
       </label>
     </div>
   `,
